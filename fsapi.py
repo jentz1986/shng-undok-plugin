2baa8a8b9caa9cf40660d8f9be757e40e558f5a0
chermes <hermes@hausmilbe.net>
2017-11-27 22:01:58 +0100

    added some comments, incl. the license header

--- fsapi.py
+++ fsapi.py
@@ -1,4 +1,17 @@
-#/usr/bin/env python
+# Copyright 2014 Dan Krause
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 from lxml import objectify
 
@@ -38,8 +51,8 @@ class FSAPI(object):
     }
 
     def __init__(self, fsapi_device_url, pin):
-        self.pin = pin
-        self.sid = None
+        self.pin = pin  # pin
+        self.sid = None  # session ID
         self.webfsapi = None
         self.fsapi_device_url = fsapi_device_url
 
